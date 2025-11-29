f6368dcd379b49a91d3b8ec669bb4ca806762599
The1CoolCoder <141706943+The1CoolCoder@users.noreply.github.com>
2025-11-29 12:43:14 -0800

    Add files via upload

---
+++ pkmn.js
@@ -0,0 +1,56 @@
+const pokemonCount = 200;
+var pokedex = {}; //{1: {name: "bulbasaur", "img" type: ["grass", "poison"], description}}
+
+
+window.onload =  async function(){
+   // getPokemon(1);
+    for (let i = 1; i <= pokemonCount; i++) {
+        await getPokemon(i);
+        let pokemon = document.createElement("div");
+        pokemon.id = i;
+        pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+        pokemon.classList.add("pokemon-name");
+        pokemon.addEventListener("click", updatePokemon);
+        document.getElementById("pokemon-list").appendChild(pokemon);
+    }
+    document.getElementById("pokemon-description").innerHTML = pokedex[1]["desc"];
+}
+
+async function getPokemon(num) {
+  let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+  let res = await fetch(url);
+  let pokemon = await res.json();
+  //console.log(pokemon);
+  let pokemonName = pokemon["name"];
+  let pokemonType = pokemon["types"];
+  let pokemonImg = pokemon["sprites"]["front_default"];
+  res = await fetch(pokemon["species"]["url"]);
+  let pokemonDesc = await res.json();
+  //console.log(pokemonName);
+  pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"];
+  pokedex[num] = {"name": pokemonName, 
+                 "img": pokemonImg, 
+                 "types": pokemonType, 
+                 "desc" : pokemonDesc
+                }
+}
+
+function updatePokemon(){
+    document.getElementById("pokemon-img").src = pokedex[this.id]["img"];
+    //clear previous info
+    let typesDiv = document.getElementById("pokemon-types");
+    while(typesDiv.firstChild){
+        typesDiv.firstChild.remove();
+    }
+    //update types
+    let types = pokedex[this.id]["types"];
+    for (let i = 0; i < types.length; i++){
+        let type = document.createElement("span");
+        type.innerText = types[i]["type"]["name"].toUpperCase();
+        type.classList.add("type-box");
+        type.classList.add(types[i]["type"]["name"]);//adds background and font color
+        typesDiv.appendChild(type);
+    }
+    //update description 
+    document.getElementById("pokemon-description").innerHTML = pokedex[this.id]["desc"];
+}
